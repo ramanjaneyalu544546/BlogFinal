61a308ff8e6bbd53a3847c798a9da587495a5f88
ramanjaneyalu <ramanjaneyaluvenna@gmail.com>
2025-12-05 07:17:15 +0000

    updated

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,5 +18,23 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Import the functions you need from the SDKs you need
+/*ort { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: 'mern-auth-1c4ae.firebaseapp.com',
+      projectId: 'mern-auth-1c4ae',
+        storageBucket: 'mern-auth-1c4ae.appspot.com',
+          messagingSenderId: '277641423672',
+            appId: '1:277641423672:web:2de25252aae022d51aafcd',
+            };
+
+            // Initialize Firebase
+            export const app = initializeApp(firebaseConfig);*/
